c6ed11747953ec8c91013bd7890a4d452b55cefc
Firastic <adyaksa.wisanggeni@gmail.com>
2020-06-16 08:44:22 +0700

    Add params checker test for balance

--- tests/models/test_balance.py
+++ tests/models/test_balance.py
@@ -1,5 +1,5 @@
 import pytest
-from xendit import Balance
+from xendit import Balance, XenditError
 from xendit.api_requestor import APIRequestor
 
 
@@ -26,8 +26,17 @@ def test_return_balance_on_correct_params(mocker, substitute_correct_get):
     assert Balance.get(Balance.AccountType.CASH).balance == 1000
 
 
-def test_raise_error_on_wrong_params(mocker, substitute_incorrect_get):
+def test_raise_xendit_error_on_response_error(mocker, substitute_incorrect_get):
     mocker.patch.object(APIRequestor, "get")
     APIRequestor.get.return_value = substitute_incorrect_get
+    with pytest.raises(XenditError):
+        print(Balance.get(Balance.AccountType.CASH).balance)
+
+
+def test_raise_value_error_on_wrong_params(mocker):
+    mocked_api_get = mocker.patch("xendit.APIRequestor.get")
+    assert (
+        not mocked_api_get.called
+    ), "Params should be checked before calling APIRequestor"
     with pytest.raises(ValueError):
         print(Balance.get("Cash").balance)
