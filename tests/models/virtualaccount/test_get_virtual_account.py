0e83d63be102cc9c6453ed29e3db27c6d5008424
Firastic <adyaksa.wisanggeni@gmail.com>
2020-06-19 17:42:21 +0700

    Add test for get virtual account

---
+++ tests/models/virtualaccount/test_get_virtual_account.py
@@ -0,0 +1,40 @@
+import pytest
+from ..base_model_test import BaseModelTest
+from .sample_response import virtual_account_response
+from xendit.models import VirtualAccount
+
+
+# fmt: off
+class TestCreateVirtualAccount(BaseModelTest):
+    @pytest.fixture
+    def default_virtual_account_data(self):
+        tested_class = VirtualAccount
+        class_name = "VirtualAccount"
+        method_name = "get"
+        http_method_name = "get"
+        params = ("5eec3a3e8dd9ea2fc97d6728",)
+        expected_correct_result = virtual_account_response()
+        return (tested_class, class_name, method_name, http_method_name, params, expected_correct_result)
+
+    @pytest.mark.parametrize("mock_correct_response", [virtual_account_response()], indirect=True)
+    def test_return_virtual_account_on_correct_params(
+        self, mocker, mock_correct_response, default_virtual_account_data
+    ):
+        self.run_success_return_test_on_xendit_instance(mocker, mock_correct_response, default_virtual_account_data)
+
+    def test_raise_xendit_error_on_response_error(
+        self, mocker, mock_error_request_response, default_virtual_account_data
+    ):
+        self.run_raises_error_test_on_xendit_instance(mocker, mock_error_request_response, default_virtual_account_data)
+
+    @pytest.mark.parametrize("mock_correct_response", [virtual_account_response()], indirect=True)
+    def test_return_virtual_account_on_correct_params_and_global_xendit(
+        self, mocker, mock_correct_response, default_virtual_account_data
+    ):
+        self.run_success_return_test_on_global_config(mocker, mock_correct_response, default_virtual_account_data)
+
+    def test_raise_xendit_error_on_response_error_and_global_xendit(
+        self, mocker, mock_error_request_response, default_virtual_account_data
+    ):
+        self.run_raises_error_test_on_global_config(mocker, mock_error_request_response, default_virtual_account_data)
+# fmt: on
