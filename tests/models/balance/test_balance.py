f741722e364010810cfcc03373d5d1c9b542ad70
Firastic <adyaksa.wisanggeni@gmail.com>
2020-06-19 17:37:58 +0700

    Disable black on test class

--- tests/models/balance/test_balance.py
+++ tests/models/balance/test_balance.py
@@ -4,6 +4,7 @@ from .sample_response import balance_response
 from xendit.models import Balance, BalanceAccountType
 
 
+# fmt: off
 class TestGetBalance(BaseModelTest):
     @pytest.fixture
     def default_balance_data(self):
@@ -13,45 +14,27 @@ class TestGetBalance(BaseModelTest):
         http_method_name = "get"
         params = (BalanceAccountType.CASH,)
         expected_correct_result = balance_response()
-        return (
-            tested_class,
-            class_name,
-            method_name,
-            http_method_name,
-            params,
-            expected_correct_result,
-        )
+        return (tested_class, class_name, method_name, http_method_name, params, expected_correct_result)
 
-    @pytest.mark.parametrize(
-        "mock_correct_response", [balance_response()], indirect=True
-    )
+    @pytest.mark.parametrize("mock_correct_response", [balance_response()], indirect=True)
     def test_return_balance_on_correct_params(
         self, mocker, mock_correct_response, default_balance_data
     ):
-        self.run_success_return_test_on_xendit_instance(
-            mocker, mock_correct_response, default_balance_data
-        )
+        self.run_success_return_test_on_xendit_instance(mocker, mock_correct_response, default_balance_data)
 
     def test_raise_xendit_error_on_response_error(
         self, mocker, mock_error_request_response, default_balance_data
     ):
-        self.run_raises_error_test_on_xendit_instance(
-            mocker, mock_error_request_response, default_balance_data
-        )
+        self.run_raises_error_test_on_xendit_instance(mocker, mock_error_request_response, default_balance_data)
 
-    @pytest.mark.parametrize(
-        "mock_correct_response", [balance_response()], indirect=True
-    )
+    @pytest.mark.parametrize("mock_correct_response", [balance_response()], indirect=True)
     def test_return_balance_on_correct_params_and_global_xendit(
         self, mocker, mock_correct_response, default_balance_data
     ):
-        self.run_success_return_test_on_global_config(
-            mocker, mock_correct_response, default_balance_data
-        )
+        self.run_success_return_test_on_global_config(mocker, mock_correct_response, default_balance_data)
 
     def test_raise_xendit_error_on_response_error_and_global_xendit(
         self, mocker, mock_error_request_response, default_balance_data
     ):
-        self.run_raises_error_test_on_global_config(
-            mocker, mock_error_request_response, default_balance_data
-        )
+        self.run_raises_error_test_on_global_config(mocker, mock_error_request_response, default_balance_data)
+# fmt: on
--- tests/models/virtualaccount/test_create_virtual_account.py
+++ tests/models/virtualaccount/test_create_virtual_account.py
@@ -4,6 +4,7 @@ from .sample_response import virtual_account_response
 from xendit.models import VirtualAccount
 
 
+# fmt: off
 class TestCreateVirtualAccount(BaseModelTest):
     @pytest.fixture
     def default_virtual_account_data(self):
@@ -13,45 +14,27 @@ class TestCreateVirtualAccount(BaseModelTest):
         http_method_name = "post"
         params = ("demo_1475459775872", "BNI", "Rika Sutanto")
         expected_correct_result = virtual_account_response()
-        return (
-            tested_class,
-            class_name,
-            method_name,
-            http_method_name,
-            params,
-            expected_correct_result,
-        )
+        return (tested_class, class_name, method_name, http_method_name, params, expected_correct_result)
 
-    @pytest.mark.parametrize(
-        "mock_correct_response", [virtual_account_response()], indirect=True
-    )
+    @pytest.mark.parametrize("mock_correct_response", [virtual_account_response()], indirect=True)
     def test_return_virtual_account_on_correct_params(
         self, mocker, mock_correct_response, default_virtual_account_data
     ):
-        self.run_success_return_test_on_xendit_instance(
-            mocker, mock_correct_response, default_virtual_account_data
-        )
+        self.run_success_return_test_on_xendit_instance(mocker, mock_correct_response, default_virtual_account_data)
 
     def test_raise_xendit_error_on_response_error(
         self, mocker, mock_error_request_response, default_virtual_account_data
     ):
-        self.run_raises_error_test_on_xendit_instance(
-            mocker, mock_error_request_response, default_virtual_account_data
-        )
+        self.run_raises_error_test_on_xendit_instance(mocker, mock_error_request_response, default_virtual_account_data)
 
-    @pytest.mark.parametrize(
-        "mock_correct_response", [virtual_account_response()], indirect=True
-    )
+    @pytest.mark.parametrize("mock_correct_response", [virtual_account_response()], indirect=True)
     def test_return_virtual_account_on_correct_params_and_global_xendit(
         self, mocker, mock_correct_response, default_virtual_account_data
     ):
-        self.run_success_return_test_on_global_config(
-            mocker, mock_correct_response, default_virtual_account_data
-        )
+        self.run_success_return_test_on_global_config(mocker, mock_correct_response, default_virtual_account_data)
 
     def test_raise_xendit_error_on_response_error_and_global_xendit(
         self, mocker, mock_error_request_response, default_virtual_account_data
     ):
-        self.run_raises_error_test_on_global_config(
-            mocker, mock_error_request_response, default_virtual_account_data
-        )
+        self.run_raises_error_test_on_global_config(mocker, mock_error_request_response, default_virtual_account_data)
+# fmt: on
