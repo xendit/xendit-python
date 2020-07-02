e0eebbf33111352d232798eca05990d80a8a645b
Firastic <adyaksa.wisanggeni@gmail.com>
2020-07-02 16:12:42 +0700

    Add all invoice feature

--- xendit/models/invoice/invoice.py
+++ xendit/models/invoice/invoice.py
@@ -19,7 +19,10 @@ class Invoice:
       - InvoiceBank
 
     Static Methods:
-      - Invoice.create (API Reference: /Create Invoices)
+      - Invoice.create (API Reference: /Create Invoice)
+      - Invoice.get (API Reference: /Get Invoice)
+      - Invoice.expire (API Reference: /Expire Invoice)
+      - Invoice.list_all (API Reference: /List All Invoice)
 
     Attributes:
       - id (str)
@@ -125,6 +128,140 @@ class Invoice:
         kwargs["body"] = body
 
         resp = _APIRequestor.post(url, **kwargs)
+        if resp.status_code >= 200 and resp.status_code < 300:
+            return Invoice(**resp.body)
+        else:
+            raise XenditError(resp)
+
+    @staticmethod
+    def get(
+        *, invoice_id, for_user_id=None, x_api_version=None, **kwargs,
+    ):
+        """Get Invoice (API Reference: Invoice/Get Invoice)
+
+        Args:
+          - invoice_id (str)
+          - **for_user_id (str)
+          - **x_api_version (str): API Version that will be used. If not provided will default to the latest
+
+        Returns:
+          Invoice
+
+        Raises:
+          XenditError
+
+        """
+        url = f"/v2/invoices/{invoice_id}"
+        headers, _ = _extract_params(
+            locals(),
+            func_object=Invoice.get,
+            headers_params=["for_user_id", "x_api_version"],
+            ignore_params=["invoice_id"],
+        )
+        kwargs["headers"] = headers
+
+        resp = _APIRequestor.get(url, **kwargs)
+        if resp.status_code >= 200 and resp.status_code < 300:
+            return Invoice(**resp.body)
+        else:
+            raise XenditError(resp)
+
+    @staticmethod
+    def expire(
+        *,
+        invoice_id,
+        for_user_id=None,
+        x_idempotency_key=None,
+        x_api_version=None,
+        **kwargs,
+    ):
+        """Cancel an already created invoice (API Reference: Invoice/Expire Invoice)
+
+        Args:
+          - invoice_id (str)
+          - **for_user_id (str)
+          - **x_idempotency_key (str)
+          - **x_api_version (str): API Version that will be used. If not provided will default to the latest
+
+        Returns:
+          Invoice
+
+        Raises:
+          XenditError
+
+        """
+        url = "/invoices/{invoice_id}/expire!"
+        headers, body = _extract_params(
+            locals(),
+            func_object=Invoice.expire,
+            headers_params=["for_user_id", "x_idempotency_key", "x_api_version"],
+            ignore_params=["invoice_id"],
+        )
+        kwargs["headers"] = headers
+        kwargs["body"] = body
+
+        resp = _APIRequestor.post(url, **kwargs)
+        if resp.status_code >= 200 and resp.status_code < 300:
+            return Invoice(**resp.body)
+        else:
+            raise XenditError(resp)
+
+    @staticmethod
+    def list_all(
+        *,
+        statuses=None,
+        limit=None,
+        created_after=None,
+        created_before=None,
+        paid_after=None,
+        paid_before=None,
+        expired_after=None,
+        expired_before=None,
+        last_invoice_id=None,
+        client_types=None,
+        payment_channels=None,
+        on_demand_link=None,
+        recurring_payment_id=None,
+        for_user_id=None,
+        x_api_version=None,
+        **kwargs,
+    ):
+        """List all Invoice based on filter (API Reference: Invoice/List All Invoice)
+
+        Args:
+          - statuses (str[])
+          - limit (int)
+          - created_after (str)
+          - created_before (str)
+          - paid_after (str)
+          - paid_before (str)
+          - expired_after (str)
+          - expired_before (str)
+          - last_invoice_id (str)
+          - client_types (str[])
+          - payment_channels (str[])
+          - on_demand_link (str)
+          - recurring_payment_id (str)
+          - **for_user_id (str)
+          - **x_api_version (str): API Version that will be used. If not provided will default to the latest
+
+        Returns:
+          Invoice
+
+        Raises:
+          XenditError
+
+        """
+        url = "/invoices/{invoice_id}/expire!"
+        headers, _ = _extract_params(
+            locals(),
+            func_object=Invoice.expire,
+            headers_params=["for_user_id", "x_api_version"],
+            ignore_params=[],
+        )
+        kwargs["headers"] = headers
+
+        resp = _APIRequestor.get(url, **kwargs)
         if resp.status_code >= 200 and resp.status_code < 300:
             return Invoice(**resp.body)
         else:
